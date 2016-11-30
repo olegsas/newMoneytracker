ff9234e8ef4b17f289ffd246e23fedd8bb32422f
Oleg <o.sas@aisnovations.com>
2016-11-30 12:36:54 +0300

    I use forEach and it works correct

--- moneytracker.js
+++ moneytracker.js
@@ -20,68 +20,17 @@ function randomByr(min, max){
 
 }
 
-function matrixArray(rows){ // function to create двумерный массив
-  var arr = new Array();
-  var i="type";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-  var i="OperationName";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-  var i="amountMin";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-  var i="amountMax";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-    var i="Currency";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-    var i="Rate";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-    var i="Period";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-    var i="Account";
-    arr[i] = new Array();
-    for(var j=0; j<rows; j++){
-      arr[i][j] = null;//пишем любой наполнитель. В простейшем случае - null
-    }
-  return arr;
-}
-var userDay = matrixArray(24);// sorry this is magic constants
-print(userDay["OperationName"][3]);
-
-
-
-/*function oneDayOfUser(){
+function oneDayOfUser(){
     var i = 0;
     db.student.find().forEach(
         function(myName){
             i++;
-            userDay[OperationName][i] = myName.OperationName;
             print("Operation Name: " + myName.OperationName);
             print("i= " + i);
         }
 
     );
-    print(userDay);
     
 }
 
-oneDayOfUser();*/
+oneDayOfUser();
