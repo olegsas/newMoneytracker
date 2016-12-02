e1d2c66c021a3059d5ab25069999d3f406be5f8c
Oleg <o.sas@aisnovations.com>
2016-12-02 14:50:01 +0300

    I try to run the application. There is a problem - some variables are not defined

--- moneytracker.js
+++ moneytracker.js
@@ -101,6 +101,10 @@ function RandomAmount(AmountMin, AmountMax, Currency){
     return result;
 }//RandomAmount returns random result 
 
+function WriteTransaction(){
+    //
+}
+
 function makeMonthlyTransactions(start_Day, Month, Year, max_day_month){// we check the list of transactions and if we have a monthly one we generate a random day and make a transaction
     //there are arrays typeA[1]...typeA[length] - for every transaction
     // if we have a full month then start_Day is 1 and if we have the first month we use the start_Day
@@ -112,11 +116,16 @@ function makeMonthlyTransactions(start_Day, Month, Year, max_day_month){// we ch
             var randomStartDay = Math.floor(Math.random()*(max_day_month-start_Day) + start_Day);
             RandomAmount(AmountMin[i], AmountMax[i],Currency[i])//returns  amount 
             // make a monthly transaction, we need to call random day
+            var Number_of_the_name_of_transaction = Math.floor((Math.random()*NUMBER_OF_CATEGORY_NAMES));//0...NUMBER-1
+            // Math.random()<1 that`s why name_of_transactions<NUMBER_OF_CATEGORY_NAMES
+            var transactionName = db.student.find({"transaction":StudentH.OperationName[i]});
+            print(transactionName);
             WriteTransaction();//we write a transaction and only we need to give a random name for it
         }
     }
 }
 function run(startDate, finishDate){// global function runs transaction generation
+    var NUMBER_OF_CATEGORY_NAMES = 4;//how many names are in one category
     var startDATE = standartDate(startDate),
         start_Day = startDATE.getDate(),
         start_Month = startDATE.getMonth(),// month is in range 0...11
@@ -131,7 +140,7 @@ function run(startDate, finishDate){// global function runs transaction generati
         print("how-many-in-month - "+max_day_month);
         print("random_day - "+randomStartDay);
         
-        makeTransactionsMonthlyFirstMonth(start_Day, start_Month, start_Year,max-day_month)// we call this function
+        makeMonthlyTransactions(start_Day, start_Month, start_Year,max_day_month)// we call this function
             //to make all monthly transactions for the first month
 
 
@@ -142,5 +151,5 @@ function run(startDate, finishDate){// global function runs transaction generati
 
 }
 
-run("1/11/2016");
+run("1/11/2010", "3/5/2014");
 
